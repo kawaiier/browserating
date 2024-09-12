048bf9daf85ee063017a1bcea3a9537e46abaa41
Serge Manvelov <kawaiier@tutanota.com>
2024-09-12 22:54:31 +0400

    Footer updated

--- app/page.js
+++ app/page.js
@@ -18,17 +18,33 @@ export default function Home() {
         <BrowserRankingList />
       </main>
       <Explanation />
-      <footer className="text-center text-sm p-8">
-        <p>
+      <footer className="text-center text-sm p-8 flex flex-col items-center gap-2 mt-8">
+        <a href="https://x.com/kawaiier101" className="text-gray-400 mx-auto">
+          <span class="[&>svg]:h-5 [&>svg]:w-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 512 512"
+            >
+              <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+            </svg>
+          </span>
+        </a>
+
+        <p className="mx-auto">
           developed by{" "}
-          <a href="kawaiier.dev" className="font-bold" target="_blank">
+          <a
+            href="kawaiier.dev"
+            className="font-bold mx-auto hover:text-blue-700"
+            target="_blank"
+          >
             kawaiier
           </a>
         </p>
-        <p>
+        <p className="mx-auto">
           <a
             href="https://github.com/kawaiier/browserating"
-            className="text-gray-400"
+            className="text-gray-400 mx-auto hover:text-blue-700"
             target="_blank"
           >
             source code
